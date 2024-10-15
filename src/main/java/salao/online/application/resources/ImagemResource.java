2a2f6e2425d12407be1da67abc56fc076724fefb
Sâmela Hostins <samela@hostins.com.br>
2024-10-14 21:08:24 -0300

    upload

--- src/main/java/salao/online/application/resources/ImagemResource.java
+++ src/main/java/salao/online/application/resources/ImagemResource.java
@@ -1,22 +1,23 @@
 package salao.online.application.resources;
 
+import java.io.InputStream;
 import java.util.Map;
 
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import salao.online.application.services.interfaces.ImagemService;
-import salao.online.domain.configuracao.ImagemUploadForm;
+
 
 @Path("/imagem")
 @Tag(name = "Endpoints do Cloudinary - Imagem")
-@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ImagemResource {
 
@@ -24,10 +25,11 @@ public class ImagemResource {
     ImagemService imagemService;
 
     @POST
+    @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadImagem(ImagemUploadForm uploadForm) {
-        // Aqui você deve chamar seu serviço de imagem passando os dados recebidos
-        String url = imagemService.uploadImagem(uploadForm.getImageBytes(), uploadForm.getNomeArquivo());
+    public Response uploadImagem(@FormDataParam("imageBytes") InputStream imageBytes,
+            @FormDataParam("nomeArquivo") String nomeArquivo) {
+        String url = imagemService.uploadImagem(imageBytes, nomeArquivo);
 
         if (url != null) {
             return Response.ok(Map.of("url", url)).build();
@@ -35,4 +37,5 @@ public class ImagemResource {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
     }
+
 }

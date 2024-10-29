d55494559849a6704c0a1a73e354338afaf6310e
Sâmela Hostins <samela@hostins.com.br>
2024-10-29 16:24:19 -0300

    configDoCloud

--- src/main/java/salao/online/application/resources/ImagemResource.java
+++ src/main/java/salao/online/application/resources/ImagemResource.java
@@ -17,7 +17,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import salao.online.application.services.interfaces.ImagemService;
